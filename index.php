728fb7720d43f6426e4d9c9e321e29f3203511e1
vicxtunespro <124519343+vicxtunespro@users.noreply.github.com>
2023-03-18 00:36:14 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+<meta charset="UTF-8">
+
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<!-- Latest compiled and minified CSS -->
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+
+<!-- Latest compiled JavaScript -->
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+
+<title>Document</title>
+
+</head>
+
+<body>
+
+<div class="container-fluid d-flex justify-content-center align-items-center" style="height: 80vh; background: url(bg.jpg);">
+
+<div class="jumbotron text-center">
+
+<h1 class="display2">Contact Web App</h1>
+
+<h5><i>Keep ur contacts organised all time</i></h5>
+
+<button class="btn btn-primary"><a href="home.html" class="text-light">Start Now</a></button>
+
+</div>
+
+
+</div>
+
+</body>
+
+</html>
+
